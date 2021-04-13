376099e58c0c7b8da3ce7a122370c41e4f8b6dc3
Gabriel Padilha <gabrielpadilhasantos@gmail.com>
2021-04-12 23:33:15 -0300

    Challenge Completed

--- src/com/github/gabrielpadilh4/challenges/JavaOutputFormatting.java
+++ src/com/github/gabrielpadilh4/challenges/JavaOutputFormatting.java
@@ -13,7 +13,8 @@ public class JavaOutputFormatting {
         for (int i = 0; i < 3; i++) {
             String s1 = sc.next();
             int x = sc.nextInt();
-            //Complete this line
+
+            // Solution
             System.out.printf("%-15s",s1);
             System.out.printf("%03d",x);
             System.out.println();

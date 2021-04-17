bea2080f990efadb42c061555a33967efc5d5438
Gabriel Padilha <gabrielpadilhasantos@gmail.com>
2021-04-17 00:41:37 -0300

    Problem: Java Static Initializer Block, completed

---
+++ src/com/github/gabrielpadilh4/challenges/JavaDateAndTime.java
@@ -0,0 +1,69 @@
+package com.github.gabrielpadilh4.challenges;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
+/*
+    https://www.hackerrank.com/challenges/java-date-and-time/problem
+ */
+class Result {
+
+    /*
+     * Complete the 'findDay' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER month
+     *  2. INTEGER day
+     *  3. INTEGER year
+     */
+
+    public static String findDay(int month, int day, int year) {
+
+        Calendar c = Calendar.getInstance();
+
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month - 1);
+        c.set(Calendar.DAY_OF_MONTH, day);
+
+        String result = c.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.US);
+
+        return result.toUpperCase(Locale.ROOT);
+
+    }
+
+}
+
+
+public class JavaDateAndTime {
+
+    public static void main(String[] args) throws IOException {
+       /* BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int month = Integer.parseInt(firstMultipleInput[0]);
+
+        int day = Integer.parseInt(firstMultipleInput[1]);
+
+        int year = Integer.parseInt(firstMultipleInput[2]);
+*/
+        int month = 8;
+        int day = 5;
+        int year = 2015;
+
+        String res = Result.findDay(month, day, year);
+        System.out.println(res);
+
+       /* bufferedWriter.write(res);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close(); */
+    }
+}
+

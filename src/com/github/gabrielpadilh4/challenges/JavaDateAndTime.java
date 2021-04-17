e49cc4eb0b5b28ae234da68f7b03ccc1ccfcc6f7
Gabriel Padilha <gabrielpadilhasantos@gmail.com>
2021-04-17 00:44:01 -0300

    Problem: Java Date and Time, completed

--- src/com/github/gabrielpadilh4/challenges/JavaDateAndTime.java
+++ src/com/github/gabrielpadilh4/challenges/JavaDateAndTime.java
@@ -1,9 +1,7 @@
 package com.github.gabrielpadilh4.challenges;
 
 import java.io.*;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.GregorianCalendar;
 import java.util.Locale;
 
 /*

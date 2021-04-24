f1933dd683fb8a56c12140d64e821b430325fc29
Gabriel Padilha <gabrielpadilhasantos@gmail.com>
2021-04-23 23:34:15 -0300

    Problem: Java Regex, completed

---
+++ src/com/github/gabrielpadilh4/challenges/JavaRegex.java
@@ -0,0 +1,23 @@
+package com.github.gabrielpadilh4.challenges;
+
+import java.util.Scanner;
+
+/*
+    https://www.hackerrank.com/challenges/java-regex/problem?h_r=next-challenge&h_v=zen
+ */
+public class JavaRegex {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+    }
+}
+
+class MyRegex {
+
+    String pattern = "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+
+}
--- src/com/github/gabrielpadilh4/challenges/PatternSyntaxChecker.java
+++ src/com/github/gabrielpadilh4/challenges/PatternSyntaxChecker.java
@@ -1,2 +1,31 @@
-package com.github.gabrielpadilh4.challenges;public class PatternSyntaxChecker {
+package com.github.gabrielpadilh4.challenges;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+/*
+    https://www.hackerrank.com/challenges/pattern-syntax-checker/problem
+ */
+public class PatternSyntaxChecker {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+
+        while (testCases > 0) {
+            String pattern = in.nextLine();
+            //Write your code
+
+            try {
+                Pattern.compile(pattern);
+                System.out.println("Valid");
+                testCases--;
+            } catch (PatternSyntaxException e) {
+                testCases--;
+                System.out.println("Invalid");
+            }
+        }
+
+    }
 }
